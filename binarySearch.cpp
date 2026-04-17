fcf8a8c65dd3e3c083938b41aaacd4761de61556
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-17 15:28:38 +0700

    menambahkan if

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -88,11 +88,18 @@ void binarySearch()
             int mid = (low + high) / 2;
             if (element[mid] == x)
             {
-                cout << "\n[]Elemen" << x << "ditemukan pada indeks " << mid << "\n";
+                cout << "\n[✓]Elemen" << x << "ditemukan pada indeks " << mid << "\n";
                 return;
             }
 
             if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
         }
     }
 }
